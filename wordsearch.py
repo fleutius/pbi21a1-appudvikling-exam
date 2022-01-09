3ff81a687a68f20594e7069543bc4b96c9a3c348
fleutius <flemming88@gmail.com>
2022-01-09 14:25:12 +0100

    asd

--- wordsearch.py
+++ wordsearch.py
@@ -1,16 +1,29 @@
 import time
+import bisect
+
+words = ["Jens","Peter","Anders","Frank","Fiddo","Karsten","Mark","Eddie","Gorm","Christian","Didrik"]
+x = "Peter"
+
+def search_in(x):
+    begin = time.perf_counter_ns()
+    if x in words:
+        print("korrekt")
+    else: print("nej")
+    end = time.perf_counter_ns()
+    print(f"Programmet tog {end - begin} nanosekunder")
 
 
+def bi_find(x):
+    a = sorted(words)
+    begin = time.perf_counter_ns()
+    i = bisect.bisect_left(a, x)
+    if i != len(a) and a[i] == x:
+        print(f"{x} Findes på index: {i}")
+    else: 
+        print(x, "er fraværende")
+    end = time.perf_counter_ns()
+    print(f"Programmet tog {end - begin} nanosekunder")
 
 
-def search_in():
-    begin = time.time()
-    with open("wordlist.txt") as f:
-        
-        if "lup" in f.read():
-            print("korrekt")
-        else: print("nej")
-        end = time.time()
-        print(f"Programmet tog {end - begin}")
-
-search_in()
+search_in(x)
+bi_find(x)
