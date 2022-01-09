4ad48959791cf00c1f846a79c4c33463cfdc0dca
fleutius <flemming88@gmail.com>
2022-01-09 19:48:40 +0100

    Update ack.py

--- ack.py
+++ ack.py
@@ -6,17 +6,4 @@ def ack(m,n):
     elif m > 0 and n > 0:
         return ack(m-1, ack(m,n-1))
 
-#print(ack(3,4))
-
-
-
-def A(m, n, s ="% s"): 
-    print(s % ("A(% d, % d)" % (m, n))) 
-    if m == 0: 
-        return n + 1
-    if n == 0: 
-        return A(m - 1, 1, s) 
-    n2 = A(m, n - 1, s % ("A(% d, %% s)" % (m - 1))) 
-    return A(m - 1, n2, s) 
-  
-print("\nResult = {}".format( A(3, 4)))
+print(ack(3,4))
